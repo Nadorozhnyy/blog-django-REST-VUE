3fc9cf37de77e1d3223775252f33c6a91b72c806
Nadorozhnyy <59502289+Nadorozhnyy@users.noreply.github.com>
2024-01-13 16:44:25 +0300

    create custom user model

--- backend/blogAPI/users/models.py
+++ backend/blogAPI/users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(_("email"), blank=False, null=False, unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        CustomUser, on_delete=models.CASCADE, related_name="users"
+    )
+    name = models.CharField(max_length=30, verbose_name=_('name'))
+    surname = models.CharField(max_length=30, blank=True, verbose_name=_('surname'))
