1f7db342e5d04844009017792353b9384eeacf14
Nadorozhnyy <59502289+Nadorozhnyy@users.noreply.github.com>
2024-01-13 16:46:54 +0300

    create custom user model, register user model in admin panel

--- backend/blogAPI/users/admin.py
+++ backend/blogAPI/users/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from users.models import CustomUser, UserProfile
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ["id", "user"]
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            _("personal information"),
+            {"fields": ("username", "first_name", "last_name")},
+        ),
+        (
+            _("permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("events"), {"fields": ("last_login", "date_joined")}),
+    )
