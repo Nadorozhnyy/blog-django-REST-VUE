afcff6e872ef49c00aa1e0621c3e6eca55c36943
Nadorozhnyy <59502289+Nadorozhnyy@users.noreply.github.com>
2024-01-13 18:12:56 +0300

    registrate apps in settings, added rest framework settings in settings.py, added endpoints in urls.py

--- backend/blogAPI/blogAPI/settings.py
+++ backend/blogAPI/blogAPI/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework.authtoken",
+    "djoser",
     # custom apps
-    'users',
-    'blog',
+    "users",
+    "blog",
 ]
 
 MIDDLEWARE = [
@@ -124,3 +127,21 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": (
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+    ),
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    )
+}
+
+DJOSER = {
+    'SERIALIZERS': {
+        'user_create': 'users.serializers.CustomUserCreateMixin',
+    }
+}
